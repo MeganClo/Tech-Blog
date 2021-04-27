f52662188f48ef33bb5fb226adfe18775ddf1671
Megs <Megs.close@gmail.com>
2021-04-27 13:42:32 -0700

    setting up files/structure

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const routes = require("./routes");
+const sequelize = require("./config/connection");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3031;
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log("Now listening!"));
+});
