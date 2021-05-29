193e43796cf91ee2a80a0c0f3b14f83daba637cd
Megs <Megs.close@gmail.com>
2021-05-29 11:42:51 -0700

    omg-error was only because i needed to drop my database-face palm

--- models/index.js
+++ models/index.js
@@ -8,11 +8,15 @@ User.hasMany(Post, {
 });
 
 Post.belongsTo(User, {
-    foreignKey: "user_id"
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+
 });
 
 Comment.belongsTo(User, {
-    foreignKey: "user_id"
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+
 });
 
 Comment.belongsTo(Post, {
@@ -24,7 +28,8 @@ User.hasMany(Comment, {
 });
 
 Post.hasMany(Comment, {
-    foreignKey: "post_id"
+    foreignKey: "post_id",
+    onDelete: "CASCADE"
 });
 
 module.exports = { User, Post, Comment };
